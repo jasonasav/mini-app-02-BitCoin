58054ac2ad8a81537722935d17fcf341aa556495
jasonasav <jasonasav@gmail.com>
2019-05-09 16:42:28 -0700

    added reactcharjs 2 to application, have a demo chart loaded with fake dummy data.

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import Disclaimerfooter from './Disclaimerfooter.jsx'
+import { Bar } from 'react-chartjs-2';
+ 
 
 
 class App extends Component {
   constructor(props) {
       super(props);
       this.state = {
-        bitcoin: []
+        bitcoin: [],
+        fakeData: {labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [{
+        label: "My First dataset",
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: [0, 10, 5, 2, 20, 30, 45],
+        }]}
           
       };
       this.getBitCoinPrices = this.getBitCoinPrices.bind(this);
@@ -37,6 +46,8 @@ class App extends Component {
         <div>
             <h1>BitCoinViewer</h1>
             <Disclaimerfooter disclaimer={this.state.bitcoin.disclaimer}/>
+            <Bar data={this.state.fakeData} />
+
         </div>
         
       )
--- client/src/components/disclaimerfooter.jsx
+++ client/src/components/disclaimerfooter.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
-
 const Disclaimerfooter = ({ disclaimer }) => {
   return (
     <div>
       <footer>{disclaimer}</footer>
     </div>
   )
-
 }
 
 export default Disclaimerfooter;
+
+
+// chart file dist/Chart.bundle.min.js
