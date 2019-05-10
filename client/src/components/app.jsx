c360269ddba493bb2c100d62ac4fdd078035ff92
jasonasav <jasonasav@gmail.com>
2019-05-09 18:48:52 -0700

    cleaned up state in main app, got pricing and date info added to state to pass as props.

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,22 +1,27 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import Disclaimerfooter from './Disclaimerfooter.jsx'
+// import Chart from './chart.jsx';
 import { Bar } from 'react-chartjs-2';
  
-
+// function createDates(dates) {
+  
+// }
 
 class App extends Component {
   constructor(props) {
     super(props);
       this.state = {
         bitcoin: [],
-        fakeData: { labels: ["2019-04-09", "February", "March", "April", "May", "June", "July"],
+        fakeData: { labels: ["2019-04-09", "ggg", "ggg", "April", "May", "June", "July"],
         datasets: [{
         label: "BitCoin Closing Price",
         backgroundColor: 'rgb(255, 99, 132)',
         borderColor: 'rgb(255, 99, 132)',
-        data: [5196.985, 10, 5, 2, 20, 30, 45],
-        }]}
+        data: [5190, 6000, 2433, 2434, 4320, 3430, 4500],
+        }]},
+        dates: '',
+        prices:'',
       };
       this.getBitCoinPrices = this.getBitCoinPrices.bind(this);
   };
@@ -28,8 +33,13 @@ class App extends Component {
   getBitCoinPrices() {
     axios.get('/bitcoin')
       .then(res => {
+        var disclaimer = res.data.disclaimer
+        var dates = Object.keys(res.data.bpi)
+        var prices = Object.values(res.data.bpi)
         this.setState({
-          bitcoin: res.data,
+          bitcoin: disclaimer,
+          dates: dates,
+          prices: prices
         })
       })
       .catch(err => {
@@ -42,7 +52,8 @@ class App extends Component {
       <div>
         <h1>BitCoinViewer</h1>
         <Bar data={this.state.fakeData} />
-        <Disclaimerfooter disclaimer={this.state.bitcoin.disclaimer}/>
+        {/* <Chart dates={Object.keys(this.state.bitcoin.bpi)} price={Object.values(this.state.bitcoin.bpi)} /> */}
+        <Disclaimerfooter disclaimer={this.state.bitcoin}/>
       </div>  
     )
   };
--- client/src/components/chart.jsx
+++ client/src/components/chart.jsx
@@ -1,29 +1,38 @@
-import React from 'react';
-import { Bar } from 'react-chartjs-2';
+// import React from 'react';
+// import { Bar } from 'react-chartjs-2';
 
 
-class Chart extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    chartData: { 
-        labels: '',
-        datasets: [{
-        label: "BitCoin Closing Price",
-        backgroundColor: 'rgb(255, 99, 132)',
-        borderColor: 'rgb(255, 99, 132)',
-        data: '',
-        }]}   
-    }
-}
+// class Chart extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//     dates: [],
+//     prices: [],
+//     chartData: { 
+//         labels: [],
+//         datasets: [{
+//         label: "BitCoin Closing Price",
+//         backgroundColor: 'rgb(255, 99, 132)',
+//         borderColor: 'rgb(255, 99, 132)',
+//         data: [],
+//         }]}   
+//     }
+// }
 
-  render() {
-    return (
-      <div>
-        <Bar data={this.state.chartData} />
-      </div>  
-    )
-  }
-};
+//   render() {
+//     return (
+//       <div>
+//         <Bar data={ labels: this.state.date,
+//                     datasets: [{
+//                     label: "BitCoin Closing Price",
+//                     backgroundColor: 'rgb(255, 99, 132)',
+//                     borderColor: 'rgb(255, 99, 132)',
+//                     data: this.state.prices,
+//                     }]} 
+          // />
+//       </div>  
+//     )
+//   }
+// };
 
-export default Chart;
+// export default Chart;
