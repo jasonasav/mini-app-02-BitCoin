9eba7a1513b0ace63b0af083f465f4cd3b7674f9
jasonasav <jasonasav@gmail.com>
2019-05-10 14:05:34 -0700

    changed variable name for controller used in index.js

--- server/index.js
+++ server/index.js
@@ -2,13 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const axios =require('axios');
-const db = require('./controller.js')
+const api = require('./controller.js')
 const app = express();
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname + '/../client/dist')));
 
-app.get('/bitcoin', db.getBitcoin)
+app.get('/bitcoin', api.getBitcoin)
 
 let port = process.env.PORT || 3000;
 app.listen(port, ()=> console.log(`Listening on port: ${port}`));
